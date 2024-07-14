4881dd2c2ab6d62d22d71719f72217ae6df224aa
hsw824 <ghdtjddn0824@naver.com>
2024-07-14 21:34:14 +0900

    빌드 : merge 이용하여 dev, prod 분리하기

--- webpack.common.js
+++ webpack.common.js
@@ -2,23 +2,13 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const isProduction = process.env.NODE_ENV == 'production';
 
-const config = {
+module.exports = {
 	entry: './src/index.tsx',
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
-	},
-	devServer: {
-		open: true,
-		host: 'localhost',
-		historyApiFallback: true,
-		hot: true,
-		port: '3000',
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
@@ -26,16 +16,7 @@ const config = {
 		}),
 		// Add your plugins here
 		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
-		new ForkTsCheckerWebpackPlugin({
-			typescript: {
-				diagnosticOptions: {
-					semantic: true,
-					syntactic: true,
-				},
-			},
-		}),
-		!isProduction && new ReactRefreshWebpackPlugin(),
-	].filter(Boolean),
+	],
 	module: {
 		rules: [
 			{
@@ -59,14 +40,4 @@ const config = {
 	resolve: {
 		extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
 	},
-	devtool: 'inline-source-map',
-};
-
-module.exports = () => {
-	if (isProduction) {
-		config.mode = 'production';
-	} else {
-		config.mode = 'development';
-	}
-	return config;
 };
