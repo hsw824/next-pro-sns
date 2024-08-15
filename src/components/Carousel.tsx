861d0ae71349dd362d6bbc89fb5688c164803325
hsw824 <ghdtjddn0824@naver.com>
2024-08-15 20:45:16 +0900

    feat : 지연로딩, 자동 슬라이드 추가

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,21 +1,11 @@
 import ImageContainer from './Image';
 import styles from './Carousel.module.scss';
 import { useEffect, useRef, useState } from 'react';
-// import styles from './Carousel.module.scss';
-// 버튼 생성
-// 1. 전체 container 사이즈 조정
-// 2. ul display flex overflow hidden
-// 3. 개별 이미지 transform 적용
-// 4. 마지막일때 첫번째로 가는것, 첫번째일때 마지막으로
-// 지금 보이는것 +2개씩만 미리 로드하고 지연로딩하기
-// 지연로딩은 사실상 image 컴포넌트에서 하는건가
-// 버튼 하나씩 구현했다가 합칠 수 있게해서 합치고
 
-// 다시
-// 1번에서 왼쪽 버튼을 누르면 1~10번째거를 복사해서 1번의 앞쪽에 붙임
-// 10번에서 오른쪽 버튼을 누르면 1~10을 복사해서 10번의 뒤쪽에 붙임
-// or
-// 하나씩만 처리한다 지금 내가 보이는것만 따져서 앞 뒤를 정해준다. 내가 보여지는게 끝나면 양 옆은 지우기
+// 1. [마지막 이미지, ...imgList, 첫번째 이미지]
+// 2. 첫번째 이미지로 가면 ...imgList의 첫번째 이미지로 전환 transition을 빈값으로 줘서 바뀌는지 모르게
+
+// autoSlide에서 정한 setInterval이랑 속임수 로딩되는 setTimeout이랑 충돌이 나는것 같은데 어떻게 해결해야할지 모르겠음
 interface ImgListType {
 	id: number;
 	alt: string;
@@ -57,7 +47,7 @@ export default function Carousel({
 			setCurrentIndex(index);
 			if (!$ul.current) return;
 			$ul.current.style.transition = '';
-			startAutoSlide();
+			// startAutoSlide();
 		}, 500);
 	};
 	const handleSlider = (index: number) => {
@@ -66,7 +56,6 @@ export default function Carousel({
 		if (newIndex === imgList.length + 1) {
 			moveToSlide(1);
 		} else if (newIndex === 0) {
-			console.log('111');
 			moveToSlide(imgList.length);
 		}
 		setCurrentIndex((prev) => prev + index);
@@ -86,14 +75,14 @@ export default function Carousel({
 			handleSlider(1);
 		}, autoSlideSec * 1000);
 	};
-	useEffect(() => {
-		startAutoSlide();
-		return () => {
-			if (interval.current) {
-				clearInterval(interval.current);
-			}
-		};
-	}, [autoSlideSec]);
+	// useEffect(() => {
+	// 	startAutoSlide();
+	// 	return () => {
+	// 		if (interval.current) {
+	// 			clearInterval(interval.current);
+	// 		}
+	// 	};
+	// }, [autoSlideSec]);
 
 	return (
 		<div className={styles.container}>
@@ -106,13 +95,7 @@ export default function Carousel({
 			>
 				이전
 			</button>
-			<ul
-				className={styles.carouselWrapper}
-				ref={$ul}
-				style={{
-					transition: 'all 500ms',
-				}}
-			>
+			<ul className={styles.carouselWrapper} ref={$ul}>
 				{carouselList.map((item, index) => {
 					return (
 						<li key={index}>
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import useIntersectionObserver from '../hook/useIntersectionObserver';
+
+const ioOption: IntersectionObserverInit = {
+	root: null,
+	threshold: 0.5,
+};
 
 const MAX_TRY_COUNT = 3;
 
@@ -11,27 +17,37 @@ export default function ImageContainer({
 }) {
 	const [tryCount, setTryCount] = useState(0);
 	const [status, setStatus] = useState<'error' | 'load' | 'success'>('load');
+	const imgRef = useRef<HTMLImageElement>(null);
+	const { entries } = useIntersectionObserver(imgRef, ioOption);
+
 	useEffect(() => {
-		const img: HTMLImageElement = new Image();
+		const isVisible = entries[0].isIntersecting;
+		if (isVisible) {
+			if (!imgRef.current) return;
+			imgRef.current.src = src;
+		}
+	}, [entries, src]);
 
-		img.src = src;
+	// useEffect(() => {
+	// 	// const img: HTMLImageElement = new Image();
+	// 	if (!imgRef.current) return;
+	// 	imgRef.current.src = src;
 
-		img.onload = () => {
-			setStatus('success');
-		};
+	// 	imgRef.current.onload = () => {
+	// 		setStatus('success');
+	// 	};
 
-		img.onerror = () => {
-			if (tryCount < MAX_TRY_COUNT) {
-				setTryCount((prev) => prev + 1);
-				console.log(tryCount);
-				return;
-			}
-			setStatus('error');
-		};
-	}, [src, tryCount]);
+	// 	imgRef.current.onerror = () => {
+	// 		if (tryCount < MAX_TRY_COUNT) {
+	// 			setTryCount((prev) => prev + 1);
+	// 			return;
+	// 		}
+	// 		setStatus('error');
+	// 	};
+	// }, [src, tryCount]);
 
-	if (status === 'load') return <div>이미지 로딩중</div>;
-	if (status === 'error') return <div>잘못된 이미지</div>;
+	// if (status === 'load') return <div>이미지 로딩중</div>;
+	// if (status === 'error') return <div>잘못된 이미지</div>;
 
-	return <img src={src} alt={alt}></img>;
+	return <img alt={alt} ref={imgRef} />;
 }
