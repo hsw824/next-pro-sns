09d612501f624c72beb2064c34399b63b63ad45d
hsw824 <ghdtjddn0824@naver.com>
2024-08-18 13:21:07 +0900

    error : intersection observer가 작동되지 않음

--- src/hook/useIntersectionObserver.ts
+++ src/hook/useIntersectionObserver.ts
@@ -6,21 +6,24 @@ const useIntersectionObserver = (
 	elemRef: RefObject<Elem>,
 	options: IntersectionObserverInit,
 ) => {
-	const observerRef = useRef<IntersectionObserver | null>(null);
+	const observerRef = useRef<IntersectionObserver>();
 	const [entries, setEntries] = useState<IntersectionObserverEntry[]>([]);
 	useEffect(() => {
 		const node = elemRef.current;
-		const handleObserver = (entries: IntersectionObserverEntry[]) => {
+		const callback = (entries: IntersectionObserverEntry[]) => {
 			setEntries(entries);
 		};
+
 		if (!node) return;
-		observerRef.current = new IntersectionObserver(handleObserver, options);
+
+		observerRef.current = new IntersectionObserver(callback, options);
 		observerRef.current.observe(node);
+
 		return () => {
-			if (!observerRef.current) return;
-			observerRef.current.disconnect();
+			observerRef.current?.disconnect();
 		};
 	}, [elemRef, options]);
+
 	return { entries, observerRef };
 };
 
