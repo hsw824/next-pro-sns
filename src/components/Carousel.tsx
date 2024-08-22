dbd60f759f5b0be17db58befcee385139e5e5306
hsw824 <ghdtjddn0824@naver.com>
2024-08-22 16:48:57 +0900

    feat: carousel 완성

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -8,17 +8,35 @@ interface ImgListType {
 	src: string;
 }
 
+interface PropsType {
+	imgList: ImgListType[];
+	autoSlideSec: number;
+	isAutoCarousel: boolean;
+}
+
 export default function Carousel({
 	imgList,
 	autoSlideSec,
-}: {
-	imgList: ImgListType[];
-	autoSlideSec: number;
-}) {
+	isAutoCarousel,
+}: PropsType) {
 	const [currentIndex, setCurrentIndex] = useState(1);
 	const [carouselList, setCarouselList] = useState<ImgListType[]>([]);
 	const $ul = useRef<null | HTMLUListElement>(null);
+
 	const interval = useRef<null | NodeJS.Timeout>(null);
+
+	useEffect(() => {
+		if (isAutoCarousel) {
+			interval.current = setInterval(() => {
+				handleSlider(1);
+			}, autoSlideSec * 1000);
+		}
+		return () => {
+			if (isAutoCarousel) {
+				clearInterval(interval.current!);
+			}
+		};
+	}, []);
 
 	useEffect(() => {
 		if (imgList.length !== 0) {
@@ -36,80 +54,60 @@ export default function Carousel({
 	}, [currentIndex]);
 
 	const moveToSlide = (index: number) => {
-		if (interval.current) {
-			clearInterval(interval.current);
-		}
 		setTimeout(() => {
 			setCurrentIndex(index);
 			if (!$ul.current) return;
 			$ul.current.style.transition = '';
-			// startAutoSlide();
 		}, 500);
 	};
 	const handleSlider = (index: number) => {
-		const newIndex = currentIndex + index;
+		setCurrentIndex((prevIndex) => {
+			const newIndex = prevIndex + index;
 
-		if (newIndex === imgList.length + 1) {
-			moveToSlide(1);
-		} else if (newIndex === 0) {
-			moveToSlide(imgList.length);
-		}
-		setCurrentIndex((prev) => prev + index);
+			if (newIndex === imgList.length + 1) {
+				moveToSlide(1);
+			} else if (newIndex === 0) {
+				moveToSlide(imgList.length);
+			}
 
-		if (!$ul.current) return;
-		$ul.current.style.transition = 'all 0.5s ease-in-out';
+			if ($ul.current) {
+				$ul.current.style.transition = 'all 0.5s ease-in-out';
+			}
+			return newIndex;
+		});
 	};
-
-	const resetAutoSlide = () => {
-		if (interval.current) {
-			clearInterval(interval.current);
-		}
-	};
-
-	const startAutoSlide = () => {
-		interval.current = setInterval(() => {
-			handleSlider(1);
-		}, autoSlideSec * 1000);
-	};
-	// useEffect(() => {
-	// 	startAutoSlide();
-	// 	return () => {
-	// 		if (interval.current) {
-	// 			clearInterval(interval.current);
-	// 		}
-	// 	};
-	// }, [autoSlideSec]);
 
 	return (
 		<div className={styles.container}>
-			<button
-				className={styles.btn}
-				onClick={() => {
-					resetAutoSlide();
-					handleSlider(-1);
-				}}
-			>
-				이전
-			</button>
+			{!isAutoCarousel && (
+				<button
+					className={styles.btn}
+					onClick={() => {
+						handleSlider(-1);
+					}}
+				>
+					이전
+				</button>
+			)}
 			<ul className={styles.carouselWrapper} ref={$ul}>
 				{carouselList.map((item, index) => {
 					return (
 						<li key={index}>
-							<ImageContainer src={item.src} alt={item.alt} />
-							<span>#{index}</span>
+							<ImageContainer src={item.src} width={600} />
 						</li>
 					);
 				})}
 			</ul>
-			<button
-				className={`${styles.btn} ${styles.next}`}
-				onClick={() => {
-					resetAutoSlide();
-					handleSlider(1);
-				}}
-			>
-				다음
-			</button>
+			{!isAutoCarousel && (
+				<button
+					className={`${styles.btn} ${styles.next}`}
+					onClick={() => {
+						handleSlider(1);
+					}}
+				>
+					다음
+				</button>
+			)}
 		</div>
 	);
 }
--- src/hook/useIntersectionObserver.ts
+++ src/hook/useIntersectionObserver.ts
@@ -1,26 +1,26 @@
 import { RefObject, useEffect, useRef, useState } from 'react';
 
 type Elem = Element | null;
-
 const useIntersectionObserver = (
 	elemRef: RefObject<Elem>,
 	options: IntersectionObserverInit,
 ) => {
-	const observerRef = useRef<IntersectionObserver | null>(null);
+	const observerRef = useRef<IntersectionObserver>();
 	const [entries, setEntries] = useState<IntersectionObserverEntry[]>([]);
+
 	useEffect(() => {
 		const node = elemRef.current;
-		const handleObserver = (entries: IntersectionObserverEntry[]) => {
-			setEntries(entries);
-		};
 		if (!node) return;
-		observerRef.current = new IntersectionObserver(handleObserver, options);
+		observerRef.current = new IntersectionObserver((entries) => {
+			setEntries(entries);
+		}, options);
 		observerRef.current.observe(node);
+
 		return () => {
-			if (!observerRef.current) return;
-			observerRef.current.disconnect();
+			observerRef.current?.disconnect();
 		};
 	}, [elemRef, options]);
+
 	return { entries, observerRef };
 };
 
